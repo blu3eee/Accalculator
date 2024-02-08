5fa2535beb95e2845cc0e9eba43b0ce1c8b06481
bluesimp1102 <jackismyname7@gmail.com>
2024-02-08 14:50:17 -0800

    [tipscaclc] removed NavigationView

--- Accalculator/Views/TipsCalculator.swift
+++ Accalculator/Views/TipsCalculator.swift
@@ -40,78 +40,76 @@ struct TipsCalculator: View {
     }
     
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Enter your bill details")) {
-                    HStack {
-                        Text("Bill total")
-                        Spacer()
-                        TextField("Enter bill total", text: $inputTotalBill)
-                            .keyboardType(.decimalPad)
-                            .multilineTextAlignment(.trailing)
-                    }
-                    
-                    HStack {
-                        Text("Number of People")
-                        Spacer()
-                        Text("\(numberOfPeople)")
-                            .frame(alignment: .center)
-                        Stepper("", value: $numberOfPeople, in: 1...100).labelsHidden()
-                    }
-                    
-                    HStack {
-                        Text("Tip percentage")
-                        Spacer()
-                        Picker("Tip percentage", selection: $tipPercentage) {
-                            ForEach(tipPercentages, id: \.self) { percentage in
-                                Text("\(percentage)%")
-                            }
-                        }
-                        .pickerStyle(.wheel)
-                        .frame(maxWidth: 100, maxHeight: 100)
-                        .clipped()
-                        .labelsHidden()
-                    }
+        List {
+            Section(header: Text("Enter your bill details")) {
+                HStack {
+                    Text("Bill total")
+                    Spacer()
+                    TextField("Enter bill total", text: $inputTotalBill)
+                        .keyboardType(.decimalPad)
+                        .multilineTextAlignment(.trailing)
                 }
                 
-                Section(header: Text("Results")) {
-                    HStack {
-                        Text("Total per person")
-                        Spacer()
-                        Text("$\(totalPerPerson, specifier: "%.2f")")
-                    }
-                    
-                    HStack {
-                        Text("Total bill (with tip)")
-                        Spacer()
-                        Text("$\(totalWithTip, specifier: "%.2f")")
-                    }.foregroundColor(Color(.orange))
+                HStack {
+                    Text("Number of People")
+                    Spacer()
+                    Text("\(numberOfPeople)")
+                        .frame(alignment: .center)
+                    Stepper("", value: $numberOfPeople, in: 1...100).labelsHidden()
                 }
                 
-                Section(header: Text("Tip Variations")) {
-                    let currentIndex = tipPercentages.firstIndex(of: tipPercentage) ?? tipPercentages.count / 2
-                    let prevIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : nil
-                    let nextIndex = currentIndex + 1 < tipPercentages.count ? currentIndex + 1 : nil
-                    
-                    let indices = [prevIndex, Optional(currentIndex), nextIndex].compactMap { $0 }
-                    
-                    HStack {
-                        
-                        ForEach(indices, id: \.self) { index in
-                            let tip = tipPercentages[index]
-                            let totalPerPerson = calculateTotalPerPerson(tipPercentage: Double(tip))
-                            let totalWithTip = calculateTotalWithTip(tipPercentage: Double(tip))
-                            Spacer()
-                            VStack {
-                                Text("\(tip)% Tip")
-                                Text("$\(totalPerPerson, specifier: "%.2f")")
-                                Text("$\(totalWithTip, specifier: "%.2f")").foregroundColor(Color(.orange))
-                            }.scaledToFit()
-                            Spacer()
+                HStack {
+                    Text("Tip percentage")
+                    Spacer()
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { percentage in
+                            Text("\(percentage)%")
                         }
-                        
-                    }.multilineTextAlignment(.center)
+                    }
+                    .pickerStyle(.wheel)
+                    .frame(maxWidth: 100, maxHeight: 100)
+                    .clipped()
+                    .labelsHidden()
                 }
+            }
+            
+            Section(header: Text("Results")) {
+                HStack {
+                    Text("Total per person")
+                    Spacer()
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                HStack {
+                    Text("Total bill (with tip)")
+                    Spacer()
+                    Text("$\(totalWithTip, specifier: "%.2f")")
+                }.foregroundColor(Color(.orange))
+            }
+            
+            Section(header: Text("Tip Variations")) {
+                let currentIndex = tipPercentages.firstIndex(of: tipPercentage) ?? tipPercentages.count / 2
+                let prevIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : nil
+                let nextIndex = currentIndex + 1 < tipPercentages.count ? currentIndex + 1 : nil
+                
+                let indices = [prevIndex, Optional(currentIndex), nextIndex].compactMap { $0 }
+                
+                HStack {
+                    
+                    ForEach(indices, id: \.self) { index in
+                        let tip = tipPercentages[index]
+                        let totalPerPerson = calculateTotalPerPerson(tipPercentage: Double(tip))
+                        let totalWithTip = calculateTotalWithTip(tipPercentage: Double(tip))
+                        Spacer()
+                        VStack {
+                            Text("\(tip)% Tip")
+                            Text("$\(totalPerPerson, specifier: "%.2f")")
+                            Text("$\(totalWithTip, specifier: "%.2f")").foregroundColor(Color(.orange))
+                        }.scaledToFit()
+                        Spacer()
+                    }
+                    
+                }.multilineTextAlignment(.center)
             }
         }
     }
